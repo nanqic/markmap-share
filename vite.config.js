ae150f577302558a4fca6ee51d04ce9c1dba013b
nanqic <nanqic@163.com>
2023-12-18 10:08:46 +0800

    rollupOptions

--- vite.config.js
+++ vite.config.js
@@ -24,18 +24,18 @@ export default defineConfig({
     sourcemap: false, // 这个生产环境一定要关闭，不然打包的产物会很大
     
     // rollup 配置
-    // rollupOptions: {
-    //   output: {
-    //     manualChunks(id) {
-    //       if (id.includes("node_modules")) {
-    //         return id
-    //           .toString()
-    //           .split("node_modules/")[1]
-    //           .split("/")[0]
-    //           .toString();
-    //       }
-    //     }
-    //   }
-    // }
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes("node_modules")) {
+            return id
+              .toString()
+              .split("node_modules/")[1]
+              .split("/")[0]
+              .toString();
+          }
+        }
+      }
+    }
   }
 });
