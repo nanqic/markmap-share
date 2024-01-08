39ad87ee29ba9f09ae76ff859237169725fdc0a3
nanqic <nanqic@163.com>
2024-01-08 10:21:16 +0800

    fix: summary flex , open icon

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -35,6 +35,9 @@ export default function
         <>
             <details open>
                 <summary className='flex items-center text-green-700'>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
+                        <path fillRule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5" />
+                    </svg>
                     <Link href={`${import.meta.env.VITE_BASE_URL || '/'}`} onClick={() => setContent()}> 🏠</Link>
                     {state.username && <select onChange={handleChangeUser} value={state.username}>
                         {
