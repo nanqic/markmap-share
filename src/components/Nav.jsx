7a5549e962140c3e76e092518b734acc4d3677d9
nanqic <nanqic@163.com>
2024-01-28 19:33:02 +0800

    fix: path

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -12,7 +12,7 @@ export default function
     const params = useParams();
 
     useEffect(() => {
-
+        console.log(!params.dir || params.dir?.endsWith('.md'), params.foldername);
         if (localStorage.getItem("theme")) {
             import(`assets/theme-wheat.css`)
             setTheme('wheat')
@@ -52,7 +52,7 @@ export default function
                             })
                         }
                     </select>}
-                    <Link href={`${import.meta.env.VITE_BASE_URL}/new?path=/${params.foldername || ''}/${!params.dir || params.dir?.endsWith('.md') ? '' : params.dir + '/'}`} className='pl-3' title='新建'>
+                    <Link href={`${import.meta.env.VITE_BASE_URL}/new?path=/${params.foldername && params.foldername + '/' || ''}${!params.dir || params.dir?.endsWith('.md') ? '' : params.dir + '/'}`} className='pl-3' title='新建'>
                         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
                             <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16" />
                             <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4" />
