40ca15e69e2179299deff2633f3de8f4553df131
Amigo <nanqic@163.com>
2025-05-16 08:16:19 +0800

    fix: download html

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -69,7 +69,7 @@ export default function
                             </svg>
                         }
                     </button>
-                    {getToken() == null ?
+                    {/* {getToken() == null ?
                         import.meta.env.VITE_SERVER_URL == location.href.slice(0, -1) ?
                             < Link href='/@login' className='text-sm'>登录</Link> :
                             < Link href='#' className='text-sm' title='演示站暂不提供登录，须在alist二级下目录使用'>登录</Link>
@@ -79,7 +79,7 @@ export default function
                                 <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6m2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0m4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4m-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10s-3.516.68-4.168 1.332c-.678.678-.83 1.418-.832 1.664z" />
                             </svg>
                         </span>
-                    }
+                    } */}
                 </summary>
                 {state.dirs && <FileTree {...state} />}
             </details >
